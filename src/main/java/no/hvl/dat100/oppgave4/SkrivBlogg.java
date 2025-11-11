63c27e5515ab80ba42f4e6411634eb5c1d6beefc
Purple115 <89314203+Purple115@users.noreply.github.com>
2025-11-11 19:41:52 +0100

    Add files via upload

--- src/main/java/no/hvl/dat100/oppgave4/SkrivBlogg.java
+++ src/main/java/no/hvl/dat100/oppgave4/SkrivBlogg.java
@@ -3,13 +3,36 @@ package no.hvl.dat100.oppgave4;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 
-import no.hvl.dat100.common.TODO;
-import no.hvl.dat100.oppgave3.*;
+import no.hvl.dat100.oppgave3.Blogg;
 
 public class SkrivBlogg {
 
-	public static boolean skriv(Blogg samling, String mappe, String filnavn) {
+    public static boolean skriv(Blogg samling, String mappe, String filnavn) {
 
-		throw new UnsupportedOperationException(TODO.method());
-	}
+        PrintWriter writer = null;
+
+        try {
+            String filepath;
+
+            if (mappe == null || mappe.equals("")) {
+                filepath = filnavn;
+            } else if (mappe.endsWith("/") || mappe.endsWith("\\")) {
+                filepath = mappe + filnavn;
+            } else {
+                filepath = mappe + "/" + filnavn;
+            }
+
+            writer = new PrintWriter(filepath);
+            writer.print(samling.toString());
+            return true;
+
+        } catch (FileNotFoundException e) {
+            return false;
+
+        } finally {
+            if (writer != null) {
+                writer.close();
+            }
+        }
+    }
 }
